bfc32f9d3d29c9c38117a49acc1e01a06f4cd5dd
Rakesh <mondalrakesh461@gmail.com>
2025-04-27 10:22:49 +0530

    used stream pipe

--- scripts/stream.js
+++ scripts/stream.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
-
+/*
+// ------------------  ReadStream Example ----------------- //
 const readableStream = fs.createReadStream("example.txt", {
    //encoding:'utf8',
   highWaterMark: 4, // buffer size in bytes
@@ -12,3 +13,36 @@ readableStream.on("data", (chunk) => {
 readableStream.on("end", () => {
   console.log("data read complete!");
 });
+
+// ------------------  ReadStream Example ----------------- //
+
+
+
+
+// ------------------  WriteStream Example ----------------- //
+
+const readableStream = fs.createReadStream("example.txt", {
+  highWaterMark: 2,
+});
+
+const writeableStream = fs.createWriteStream("copytext.txt");
+
+readableStream.on('data',(chunk)=>{
+  console.log(chunk);
+  writeableStream.write(chunk);
+});
+
+// ------------------  WriteStream Example ----------------- //
+
+*/
+
+// ------------------ Use cases of Pipes ------------------ //
+
+const readableStream = fs.createReadStream("example.txt", {
+  highWaterMark: 2,
+});
+
+const writeableStream = fs.createWriteStream("copytext.txt");
+
+readableStream.pipe(writeableStream);
+
